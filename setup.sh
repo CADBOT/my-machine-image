9f28d24083f0d76673988bb5ed18ea5336d5ec34
cadbot <charliec364@gmail.com>
2015-03-08 09:46:48 +0000

    update ruby install

--- setup.sh
+++ setup.sh
@@ -34,9 +34,9 @@ cd ..
 
 # Install ruby-install
 
-wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
-tar -xzvf ruby-install-0.4.3.tar.gz
-cd ruby-install-0.4.3/
+wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
+tar -xzvf ruby-install-0.5.0.tar.gz
+cd ruby-install-0.5.0/
 sudo make install
 cd ..
 
